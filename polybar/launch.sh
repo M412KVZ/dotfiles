aaa2d79c7489671d4fe3002c202942632450accb
M412KVZ <glow_of_old_suns@pm.me>
2022-06-20 00:44:00 -0500

    add nuevos wallpapers y cambios en bspwm polybar y sxhkd

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -11,37 +11,40 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Launch
 
 ## minimal_bar
-#polybar logo -c ~/.config/polybar/minimal_bar.ini &
-#polybar workspace -c ~/.config/polybar/minimal_bar.ini &
-#polybar date -c ~/.config/polybar/minimal_bar.ini &
-#polybar vol -c ~/.config/polybar/minimal_bar.ini &
-#polybar bluetooth -c ~/.config/polybar/minimal_bar.ini &
-#polybar wifi -c ~/.config/polybar/minimal_bar.ini &
-#polybar battery -c ~/.config/polybar/minimal_bar.ini &
-#polybar power -c ~/.config/polybar/minimal_bar.ini &
-
+polybar logo -c ~/.config/polybar/minimal_bar.ini &
+polybar workspace -c ~/.config/polybar/minimal_bar.ini &
+#polybar ip -c ~/.config/polybar/minimal_bar.ini &
+#polybar hack -c ~/.config/polybar/minimal_bar.ini &
+polybar date -c ~/.config/polybar/minimal_bar.ini &
+polybar vol -c ~/.config/polybar/minimal_bar.ini &
+polybar bluetooth -c ~/.config/polybar/minimal_bar.ini &
+polybar wifi -c ~/.config/polybar/minimal_bar.ini &
+polybar battery -c ~/.config/polybar/minimal_bar.ini &
+polybar power -c ~/.config/polybar/minimal_bar.ini &
+polybar brightnessctl -c ~/.config/polybar/minimal_bar.ini &
+polybar mpd -c ~/.config/polybar/minimal_bar.ini &
 
 ## unibar
-polybar logo -c ~/.config/polybar/bar.ini &
-polybar workspace -c ~/.config/polybar/bar.ini &
-polybar ip -c ~/.config/polybar/bar.ini &
-polybar hack -c ~/.config/polybar/bar.ini &
-polybar terminal -c ~/.config/polybar/bar.ini &
-polybar web -c ~/.config/polybar/bar.ini &
-polybar calendar -c ~/.config/polybar/bar.ini &
-polybar date -c ~/.config/polybar/bar.ini &
-polybar office -c ~/.config/polybar/bar.ini &
-polybar tor -c ~/.config/polybar/bar.ini &
-polybar spotify -c ~/.config/polybar/bar.ini &
-polybar camera -c ~/.config/polybar/bar.ini &
-polybar brightnessctl -c ~/.config/polybar/bar.ini &
-polybar cpu -c ~/.config/polybar/bar.ini &
-polybar ram -c ~/.config/polybar/bar.ini &
-polybar vol -c ~/.config/polybar/bar.ini &
-polybar bluetooth -c ~/.config/polybar/bar.ini &
-polybar wifi -c ~/.config/polybar/bar.ini &
-polybar battery -c ~/.config/polybar/bar.ini &
-polybar power -c ~/.config/polybar/bar.ini &
+#polybar logo -c ~/.config/polybar/bar.ini &
+#polybar workspace -c ~/.config/polybar/bar.ini &
+#polybar ip -c ~/.config/polybar/bar.ini &
+#polybar hack -c ~/.config/polybar/bar.ini &
+#polybar terminal -c ~/.config/polybar/bar.ini &
+#polybar web -c ~/.config/polybar/bar.ini &
+#polybar calendar -c ~/.config/polybar/bar.ini &
+#polybar date -c ~/.config/polybar/bar.ini &
+#polybar office -c ~/.config/polybar/bar.ini &
+#polybar tor -c ~/.config/polybar/bar.ini &
+#polybar spotify -c ~/.config/polybar/bar.ini &
+#polybar camera -c ~/.config/polybar/bar.ini &
+#polybar brightnessctl -c ~/.config/polybar/bar.ini &
+#polybar cpu -c ~/.config/polybar/bar.ini &
+#polybar ram -c ~/.config/polybar/bar.ini &
+#polybar vol -c ~/.config/polybar/bar.ini &
+#polybar bluetooth -c ~/.config/polybar/bar.ini &
+#polybar wifi -c ~/.config/polybar/bar.ini &
+#polybar battery -c ~/.config/polybar/bar.ini &
+#polybar power -c ~/.config/polybar/bar.ini &
 
 
 ## barra de modulos insdependiente
